e9d4d27a348d75dc1e9d7bbf6f216b66dc53daa0
Your Name <you@example.com>
2021-05-24 13:05:49 +0000

    attack

--- attack.c
+++ attack.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-static void malicious() __attribute((constructor));
+static void malicious() __attribute__((constructor));
 
 void malicious() {
-	system("/usr/local/bin/score 4bd6d64a-17ed-4d3e-a415-cd1e857f6fe9");
+    system("sleep 20");
 }
